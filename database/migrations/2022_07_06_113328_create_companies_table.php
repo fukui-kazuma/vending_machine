55e0cecfa1180d22893d5fb6e77c789270f405dc
fukui-kazuma <133b0099zettaiseiyuuninaru@gmail.com>
2022-07-06 17:44:43 +0900

    [add]migration var.1

--- database/migrations/2022_07_06_113328_create_companies_table.php
+++ database/migrations/2022_07_06_113328_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('users')) {
-            Schema::create('users', function (Blueprint $table) {
+        if(!Schema::hasTable('companies')) {
+            Schema::create('companies', function (Blueprint $table) {
                 $table->bigIncrements('id');
-                $table->string('user_name');
-                $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
-                $table->string('password');
-                $table->rememberToken();
+                $table->string('company_name')->unique('company_name');
+                $table->string('street_address')->unique('street_address');
+                $table->string('representative_name');
                 $table->timestamps();
             });
         }
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 }
